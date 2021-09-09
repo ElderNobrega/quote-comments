5f1b72d64e5bec6a42c8993fe88f02b2d1d2018c
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-09-09 19:12:07 -0400

    Basics of routing

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom"
+
 const Products = () => {
-  return <h1>Products Page</h1>
+  return (
+    <section>
+      <h1>Products 1</h1>
+      <ul>
+        <li><Link to='/products/p1'>Book</Link></li>
+        <li><Link to='/products/p2'>Carpet</Link></li>
+        <li><Link to='/products/p3'>Online Course</Link></li>
+      </ul>
+    </section>
+  )
 }
 
 export default Products
