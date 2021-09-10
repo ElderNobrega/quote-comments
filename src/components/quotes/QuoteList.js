b4c7ddd5781d1c640fbbe4971ed75a2f8a5db8f4
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-09-10 12:25:10 -0400

    path name more dinamic

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -26,7 +26,10 @@ const QuoteList = (props) => {
   const sortedQuote = sortQuotes(props.quotes, isSortingAscending)
 
   const changeSortingHandler = () => {
-    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'))
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${(isSortingAscending ? 'desc' : 'asc')}`
+    })
   }
 
   return (
